8d25364ca965fa8984e32fc038d90ea99223ab80
Manne Rithvk <mannerithvik708@gmail.com>
2024-08-23 17:17:37 +0530

    lovecommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-r from-purple-500 to-pink-500">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24 bg-gradient-to-r from-purple-500 to-pink-500">
       <Quiz/>
     </main>
   );
